ab775e75d08c9c9d6a76d68acda8a822f42c4bad
Reva Gupta <revagupta95@gmail.com>
2018-06-09 12:21:13 +0530

    Fourth commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,40 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ChatModule } from './chat/chat.module';
+
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AngularFireModule } from 'angularfire2';
+
+// New imports to update based on AngularFire2 version 4
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+import { PhoneLoginComponent } from './phone-login/phone-login.component';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDEMIMCLhWBJT065AQ5fGPridxi1ACPuXo",
+  authDomain: "chatbot-8dd24.firebaseapp.com",
+  databaseURL: "https://chatbot-8dd24.firebaseio.com",
+  storageBucket: "chatbot-8dd24.appspot.com",
+  messagingSenderId: "3666865050"
+};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PhoneLoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ChatModule,
+    FormsModule,
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/chat/chat-dialog/chat-dialog.component.ts
@@ -0,0 +1,84 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'chat-dialog',
+  templateUrl: './chat-dialog.component.html',
+  styleUrls: ['./chat-dialog.component.css']
+})
+export class ChatDialogComponent implements OnInit {
+  questionNum = 0;	// keep count of question, used for IF condition.
+  count=0;
+  question = 'What is your name?';
+  amt1;
+  amt2;
+  amt3;
+   
+				  // first question
+  output = document.getElementById('output'); // store id="output" in output variable
+constructor() { }
+
+  ngOnInit() {
+  }
+  bot() { 
+    var input = (<HTMLInputElement>document.getElementById("input")).value;
+    console.log(input);
+    alert("Hello!  "+input);
+    var output = document.getElementById('output');
+    this.question = '<h1>Enter 1st bill?</h1>';	
+    var output = document.getElementById('output');			
+     output.innerHTML = this.question;   
+    (<HTMLInputElement>document.getElementById("input")).value = "";   		// clear text box
+}
+ bot1() { 
+  this.question = '<h1>Enter 2nd bill?</h1>';	
+  var output = document.getElementById('output');			
+   output.innerHTML = this.question;
+   this.amt1 = (<HTMLInputElement>document.getElementById("input")).value;
+   this.amt1=parseInt(this.amt1,10);
+  console.log(this.amt1);
+  //output.innerHTML = '<h1>1 st bill ' + amt1 + '</h1>';// output response
+  (<HTMLInputElement>document.getElementById("input")).value = "";   		// clear text box
+  }   
+   bot2() { 
+    this.question = '<h1>Enter 3rd bill?</h1>';	
+    var output = document.getElementById('output');			
+     output.innerHTML = this.question;
+     this.amt2 = (<HTMLInputElement>document.getElementById("input")).value;
+     this.amt2=parseInt(this.amt2,10);
+    console.log(this.amt2);
+    //output.innerHTML = '<h1>1 st bill ' + amt2 + '</h1>';// output response
+    (<HTMLInputElement>document.getElementById("input")).value = "";   		// clear text box
+    } 
+    bot3() { 
+      this.question = '<h1>Do you want to know the best plan?</h1>';	
+      var output = document.getElementById('output');			
+       output.innerHTML = this.question;
+       this.amt3 = (<HTMLInputElement>document.getElementById("input")).value;
+       this.amt3=parseInt(this.amt3,10);
+      console.log(this.amt3);
+     // output.innerHTML = '<h1>1 st bill ' + amt3 + '</h1>';// output response
+      (<HTMLInputElement>document.getElementById("input")).value = "";   		// clear text box
+      } 
+      avg()
+      {   var amt4=(this.amt1+this.amt2+this.amt3)/3.0;
+          this.output.innerHTML = '<h1>Average bill amount is ' + amt4 + '</h1>';
+          (<HTMLInputElement>document.getElementById("input")).value = "";
+      }
+  
+  checkkey(event)
+  { switch (this.count)
+      {case 0: 
+    console.log("bot");  this.bot();  this.count++;  break;
+    case 1: 
+    console.log("bot1");  this.bot1();  this.count++;  break;
+    case 2: 
+    console.log("bot2");  this.bot2();  this.count++;  break;
+    case 3: 
+    console.log("bot3");  this.bot3();  this.count++;  break;
+    case 4: 
+    console.log("avg");  this.avg();  this.count++;  break;
+      }
+    }
+   
+  
+}
